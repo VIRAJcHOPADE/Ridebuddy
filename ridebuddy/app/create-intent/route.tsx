ca3993e856b61f5a347cefb54cc5a621cbb53902
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-09-16 16:30:59 +0530

    Update route.tsx

--- ridebuddy/app/create-intent/route.tsx
+++ ridebuddy/app/create-intent/route.tsx
@@ -1,1 +1,27 @@
 
+import {NextResponse} from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe('sk_test_51N5qBmSGzCFMjSOkFkGbGHHoN9o8jPM9y9Fsy1lIIqdaUWErMGczIVzOwDJf0OyGJLVhJO7NxbGs0tEkxPKOgq7P00tRVtXhFf'!,{
+    typescript:true,
+    apiVersion:"2023-08-16"
+})
+
+export async function POST(request:any){
+    
+    const data:any=await request.json();
+    const amount=data.amount;
+
+    try{
+        const paymentIntent=await stripe.paymentIntents.create({
+            amount:Number(amount)*100,
+            currency:'USD'
+        })
+        return NextResponse.json(paymentIntent.client_secret,{status:200})
+    }
+    catch(error:any){
+         return new NextResponse(error,{
+            status:400,
+         });
+    }
+}
