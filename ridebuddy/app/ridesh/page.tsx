632fb4238a77e7ede86bdd48a4dbc8cb2aaab25d
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-09-17 10:16:27 +0530

    Update page.tsx

--- ridebuddy/app/ridesh/page.tsx
+++ ridebuddy/app/ridesh/page.tsx
@@ -1,1 +1,14 @@
+"use client";
+import React,{ useContext } from 'react';
+// import Rides from '@/components/Ridesharing/rides'; // Assuming 'rides' is the exported component name.
+import Ridesharesh from '@/components/Ridesharing/ridesharinges'
+const Ridesh = () => {
+  return (
+    <div>
+      <Ridesharesh/>
+    </div>
+  );
+}
+
+export default Ridesh;
 
