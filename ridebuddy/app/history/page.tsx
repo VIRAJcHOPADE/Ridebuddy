016afda75147bb91680c0a8ab6c66f178946596d
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-09-17 10:15:32 +0530

    Update page.tsx

--- ridebuddy/app/history/page.tsx
+++ ridebuddy/app/history/page.tsx
@@ -1,1 +1,15 @@
 
+"use client";
+import React,{ useContext } from 'react';
+// import Rides from '@/components/Ridesharing/rides'; // Assuming 'rides' is the exported component name.
+import History from '@/components/Ridesharing/History'
+
+const Ridehistory = () => {
+  return (
+    <div>
+        <History/>
+    </div>
+  )
+}
+
+export default Ridehistory
